569216619e828ef43bb35019f5b9c349a581a19d
claire-graham <160625788+claire-graham@users.noreply.github.com>
2024-09-26 14:49:21 +0000

    functions

--- server/index.js
+++ server/index.js
@@ -34,3 +34,46 @@ app.get('/badges', async (req, res) => {
     }
   });
 
+  app.delete('/badges/:id', async (req, res) => {
+    try {
+
+      // Extract id parameter from the request object
+      const { id } = req.params; 
+      const badge = await pool.query(
+        `delete from badges_list where id =$1`, [id]);
+      res.json(badge.rows[0]);
+    }catch (err) {
+      res.status(500).send('Internal error:'+ err); 
+    }
+  });
+
+  app.put('/badges/:id', async (req, res) => {
+    try {
+
+      // Extract id parameter from the request object
+        const { id } = req.params; 
+
+        // Extract the new title, and description submitted from the form.
+        const { title, description} = req.body; 
+      const badge = await pool.query(
+        `update badges_list set title=$2, description=$3 where id =$1`, [id, title, description]);
+      res.json(badge.rows[0]);
+    }catch (err) {
+    res.status(500).send('Internal error:'+ err); 
+    }
+  });
+
+  app.post('/badges', async (req, res) => {
+    try {
+      const { title, description } = req.body;  
+
+      const newBadge = await pool.query(
+        `INSERT INTO badges_list (title, description) VALUES($1, $2) RETURNING *`,
+        [title, description]
+      );
+      res.json(newBadge.rows[0]);
+
+    } catch (err) {
+    res.status(500).send('Internal error:'+ err); 
+    }
+  });
