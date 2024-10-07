c11b496c15b45c2d1fe1636081c1b4e3e41fc917
claire-graham <160625788+claire-graham@users.noreply.github.com>
2024-10-07 14:47:25 +0000

    add badge form

--- client/src/App.js
+++ client/src/App.js
@@ -9,3 +9,19 @@ return (
 }
 
 export default App;
+
+import AddBadgeForm from './components/AddBadgeForm';
+
+import { Layout } from 'antd';
+const { Content } = Layout;
+
+function App() {
+  return (
+   
+    <Content style={{ padding: '0 150px', background: 'white' }}>
+      <AddBadgeForm />
+    </Content>
+ 
+  );
+}
+export default App;
